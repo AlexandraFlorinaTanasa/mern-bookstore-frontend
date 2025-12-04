98c178edec8121ef37ee00aedc7ec60fc34c2205
Alexandra Florina Tanasă <160071375+AlexandraFlorinaTanasa@users.noreply.github.com>
2025-12-04 20:11:37 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BookCatalog from "./components/BookCatalog";
+import PublicBooks from "./components/PublicBooks";
+import PaymentSuccess from "./components/PaymentSuccess";
+import AdminLogin from "./components/admin/AdminLogin";
+// import  './App.css';
+
+const App = () => {
+   return (
+      <Router>
+         <Routes>
+            <Route path="/" element={<BookCatalog />} />
+            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/publicbooks" element={<PublicBooks />} />
+            <Route path="/payment-success" element={<PaymentSuccess />} />
+         </Routes>
+      </Router>
+   );
+};
+
+export default App;
